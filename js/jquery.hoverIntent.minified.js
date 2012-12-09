3e5732328c69f40506498d453b1791e8f6cddae9
Nguyen Minh Tam <tamnm1310@gmail.com>
2012-12-09 11:39:35 +0700

    finish trang chung tu

---
+++ js/jquery.hoverIntent.minified.js
@@ -0,0 +1,9 @@
+/**
+* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
+* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
+* 
+* @param  f  onMouseOver function || An object with configuration options
+* @param  g  onMouseOut function  || Nothing (use configuration options object)
+* @author    Brian Cherne brian(at)cherne(dot)net
+*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(3($){$.H.K=3(f,g){4 c={D:7,o:J,A:0};c=$.y(c,g?{x:f,t:g}:f);4 d,l,6,8;4 h=3(a){d=a.z;l=a.B};4 i=3(a,b){b.2=q(b.2);5((u.v(6-d)+u.v(8-l))<c.D){$(b).w("m",h);b.9=1;r c.x.s(b,[a])}C{6=d;8=l;b.2=p(3(){i(a,b)},c.o)}};4 j=3(a,b){b.2=q(b.2);b.9=0;r c.t.s(b,[a])};4 k=3(e){4 a=E.y({},e);4 b=F;5(b.2){b.2=q(b.2)}5(e.I=="G"){6=a.z;8=a.B;$(b).n("m",h);5(b.9!=1){b.2=p(3(){i(a,b)},c.o)}}C{$(b).w("m",h);5(b.9==1){b.2=p(3(){j(a,b)},c.A)}}};r F.n(\'G\',k).n(\'L\',k)}})(E);',48,48,'||hoverIntent_t|function|var|if|pX||pY|hoverIntent_s||||||||||||cY|mousemove|bind|interval|setTimeout|clearTimeout|return|apply|out|Math|abs|unbind|over|extend|pageX|timeout|pageY|else|sensitivity|jQuery|this|mouseenter|fn|type|300|hoverIntent|mouseleave'.split('|'),0,{}))
--- js/utils.js
+++ js/utils.js
@@ -2,16 +2,37 @@
  * Quy uoc: cac ham bat dau voi _ deu la cac ham xu ly giao dien
  */
 
+var popup_w = 1000,
+	popup_h = 800;
+var main_biz_delay = 900000; // 15 min
+
 $(document).ready(function(){
 
 	initSuperfishMenu();
+	initTopUse();
 	
 	addOnLoadEvent(function(){
 		_addSpanDeco();
 		_addTopNAVActiveIndicator();
+		_preventHashAnchor();
 	});
 });
 
+function initTopUse(){
+	$('#top_use > ul > li').click(function(e){
+		var href = $(this).attr('data-href');
+		_openNewWindow(href);
+		
+		e.preventDefault();
+	});
+	
+	$('#top_use > ul > li > .remove').click(function(e){
+		var parent = $(this).parents().get(0);
+		$(parent).remove();
+		
+		e.preventDefault();
+	});
+}
 
 function initSuperfishMenu(){
 	var sf = $('.sf-menu');
@@ -21,13 +42,28 @@ function initSuperfishMenu(){
 		
 		// init SUPERFISH
 		sf.superfish({ 
-			animation: {height:'show'},
-			delay: 80000, // don't hide menu when mouse-out
-			disableHI: true,
-			dropShadows: false
+			delay: main_biz_delay, // don't hide menu when mouse-out within 15'
+			dropShadows: false,
+			speed: 'fast',
+			onInit: function(){
+				$(' > li > .sub > .deco_top', this).append('<span class="left_arr"></span>');
+			}
 		}); 
+		
+		$('.sf-menu .sub .make_shortcut').click(function(e){
+			alert('click me');
+		});
 	}
 }
+
+function _openNewWindow(href){
+	var left = (screen.width/2)-(popup_w/2);
+	var top = (screen.height/2)-(popup_h/2);
+	var _option = "toolbar=0,location=0,menubar=0,status=0,scrollbars=yes,resizable=1,location=0,width="+popup_w+",height="+popup_h+",top="+top+",left="+left;
+
+	window.open(href, '_blank', _option);
+}
+
 function _addSpanDeco(){
 	$('.add_span').each(function(){
 		var obj = $(this);
@@ -45,6 +81,11 @@ function _addTopNAVActiveIndicator(){
 	$('#top .active').append('<i></i>');
 }
 
+function _preventHashAnchor() {
+	$('a[href="#"]').click(function(e){
+		e.preventDefault();
+	});
+}
 // Add event to window.onload so they don't overwrite each other. 
 // Given func will be executed after content is loaded
 function addOnLoadEvent(func){
